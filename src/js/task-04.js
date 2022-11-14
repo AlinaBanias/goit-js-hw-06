ceb76d32e6126e463cf4347b86b68ad0f1117259
AlinaBanias <isabella08092001@gmail.com>
2022-11-14 20:18:52 +0200

    add

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,2 +1,28 @@
-const divEl = document.querySelector("#counter");
+const counter = {
+    counterValue: 0,
+    increment() {
+        console.log('increment -> this' , this);
+        this.counterValue += 1;
+    },
+    decrement() {
+        console.log('decrement -> this' , this);
+        this.counterValue -= 1;
+    },
+};
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', function() {
+    counter.decrement();
+    console.log(counter);
+    valueEl.textContent = counter.counterValue;
+});
+
+incrementBtn.addEventListener('click', function() {
+    counter.increment();
+    console.log(counter);
+    valueEl.textContent = counter.counterValue;
+});
 
