7d8ccbc0063e9031c2cce25d44636fae4e415e56
AlinaBanias <isabella08092001@gmail.com>
2022-11-16 14:59:00 +0200

    task 1 and task 9

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,9 @@
-const Numbersofcategories = document.querySelectorAll("#categories");
-console.log('Number of categories :', Numbersofcategories.children.length);
+const numbersOfCategories = document.querySelector("ul");
+console.log('Number of categories:', numbersOfCategories.children.length);
 
-const category1 = Numbersofcategories[0].firstElementChild;
-console.log('Category : ' , category1.textContent);
-const listAlleys = category1.nextElementSibling;
-
-const childOfList = listAlleys.children.length;
-console.log('Elements : ', childOfList);
-
-const category2 = Numbersofcategories[1].firstElementChild;
-console.log('Category : ', category2.textContent);
-const listAlleys2 = category2.nextElementSibling;
-
-const childOfList2 = listAlleys2.children.length;
-console.log('Elements : ', childOfList2);
-
-const category3 = Numbersofcategories[2].firstElementChild;
-console.log('Category : ', category3.textContent);
-const listAlleys3 = category3.nextElementSibling;
-
-const childOfList3 = listAlleys3.children.length;
-console.log('Elements : ', childOfList3);
+for (const item of numbersOfCategories.children) { 
+     const titleEl = item.firstElementChild;  
+     console.log('Category:', titleEl.textContent);  
+     const itemsEl = item.lastElementChild;  
+     console.log('Elements:', itemsEl.children.length);
+    }
